396450d13c0ade090c12d28c49da2ff8f1f12317
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2014-11-12 20:03:47 +0000

    Disable pylint warning about star-args.
    The only use of star-args here is where it is necessary.  If it
    weren't, it wouldn't be used.

--- bayeslite.py
+++ bayeslite.py
@@ -14,6 +14,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+# pylint: disable=star-args
+
 # Implementation notes:
 #
 # - Use SQL parameters to pass strings and other values into SQL.
