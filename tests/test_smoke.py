c74042185122f4d8eb41712326de7ff7a3d59faa
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2014-12-16 22:17:36 +0000

    Add some automatic smoke tests.

---
+++ tests/test_smoke.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+import contextlib
+import pytest
+import sqlite3
+import tempfile
+
+import crosscat.CrossCatClient
+
+import bayeslite
+
+def local_crosscat():
+    return crosscat.CrossCatClient.get_CrossCatClient('local', seed=0)
+
+@contextlib.contextmanager
+def bayesdb(engine=None, **kwargs):
+    if engine is None:
+        engine = local_crosscat()
+    bdb = bayeslite.BayesDB(engine, **kwargs)
+    try:
+        yield bdb
+    finally:
+        bdb.close()
+
+def test_openclose():
+    with bayesdb():
+        pass
+
+def test_bad_db_application_id():
+    with tempfile.NamedTemporaryFile(prefix='bayeslite') as f:
+        with sqlite3.connect(f.name, isolation_level=None) as db:
+            db.execute('PRAGMA application_id = 42')
+            db.execute('PRAGMA user_version = 1')
+        with pytest.raises(IOError):
+            with bayesdb(pathname=f.name):
+                pass
+
+def test_bad_db_user_version():
+    # XXX Would be nice to avoid a named temporary file here.  Pass
+    # the sqlite3 database connection in?
+    with tempfile.NamedTemporaryFile(prefix='bayeslite') as f:
+        with sqlite3.connect(f.name, isolation_level=None) as db:
+            db.execute('PRAGMA application_id = 1113146434')
+            db.execute('PRAGMA user_version = 42')
+        with pytest.raises(IOError):
+            with bayesdb(pathname=f.name):
+                pass
+
+@contextlib.contextmanager
+def sqlite_bayesdb_table(mkbdb, name, schema, data, **kwargs):
+    with mkbdb as bdb:
+        schema(bdb)
+        data(bdb)
+        bayeslite.bayesdb_import_sqlite_table(bdb, name, **kwargs)
+        sql = 'select id from bayesdb_table where name = ?'
+        table_id = bayeslite.sqlite3_exec_1(bdb.sqlite, sql, (name,))
+        assert table_id == 1
+        yield bdb, table_id
+
+@contextlib.contextmanager
+def analyzed_bayesdb_table(mkbdb, nmodels, nsteps):
+    with mkbdb as (bdb, table_id):
+        bayeslite.bayesdb_models_initialize(bdb, table_id, nmodels)
+        for modelno in range(nmodels):
+            bayeslite.bayesdb_models_analyze1(bdb, table_id, modelno, nsteps)
+        yield bdb, table_id
+
+def t0_schema(bdb):
+    bdb.sqlite.execute('create table t0 (id integer primary key)')
+def t0_data(bdb):
+    bdb.sqlite.executemany('insert into t0 (id) values (?)',
+        [(0,), (1,), (42,)])
+
+def t0():
+    return sqlite_bayesdb_table(bayesdb(), 't0', t0_schema, t0_data)
+
+def t1_schema(bdb):
+    bdb.sqlite.execute('''
+        create table t1 (
+            id integer primary key,
+            label text,
+            age double,
+            weight double
+        )
+    ''')
+def t1_data(bdb):
+    bdb.sqlite.executemany('insert into t1 (label,age,weight) values (?,?,?)',
+        t1_rows)
+
+t1_rows = [
+    ('foo', 12, 24),
+    ('bar', 14, 28),
+    (None, 10, 20),
+    ('baz', None, 32),
+    ('quux', 4, None),
+    ('zot', 8, 16),
+    ('mumble', 8, 16),
+    ('frotz', 8, 16),
+    ('gargle', 8, 16),
+    ('mumph', 8, 16),
+    ('hunf', 11, 22),
+    ('blort', 16, 32),
+    (None, 16, 32),
+    (None, 17, 34),
+    (None, 18, 36),
+    (None, 19, 38),
+    (None, 20, 40),
+    (None, 21, 42),
+    (None, 22, 44),
+    (None, 23, 46),
+    (None, 24, 48),
+    (None, 25, 50),
+    (None, 26, 52),
+    (None, 27, 54),
+    (None, 28, 56),
+    (None, 29, 58),
+    (None, 30, 60),
+    (None, 31, 62),
+]
+
+def t1():
+    return sqlite_bayesdb_table(bayesdb(), 't1', t1_schema, t1_data)
+
+def t1_sub():
+    return sqlite_bayesdb_table(bayesdb(), 't1', t1_schema, t1_data,
+        column_names=['id', 'label', 'age'])
+
+def t1_subcat():
+    return sqlite_bayesdb_table(bayesdb(), 't1', t1_schema, t1_data,
+        column_names=['id', 'label', 'age'],
+        column_types={
+            'id': 'categorical',
+            'label': 'key',
+            'age': 'categorical',
+        })
+
+btable_generators = {
+    't0': t0,
+    't1': t1,
+    't1_sub': t1_sub,
+    't1_subcat': t1_subcat,
+}
+
+@pytest.mark.parametrize('btable_name', btable_generators.keys())
+def test_btable(btable_name):
+    with btable_generators[btable_name]():
+        pass
+
+@pytest.mark.parametrize('btable_name', btable_generators.keys())
+def test_btable_analysis0(btable_name):
+    with analyzed_bayesdb_table(btable_generators[btable_name](), 1, 0):
+        pass
+
+@pytest.mark.parametrize('btable_name', btable_generators.keys())
+def test_btable_analysis1(btable_name):
+    if btable_name == 't0':
+        pytest.xfail("Crosscat can't handle a table with only one column.")
+    with analyzed_bayesdb_table(btable_generators[btable_name](), 1, 1):
+        pass
+
+@pytest.mark.parametrize('row_id,colno,confidence',
+    [(i+1, j, conf)
+        for i in range(min(5, len(t1_rows)))
+        for j in range(3)
+        for conf in [0.01, 0.5, 0.99]])
+def test_t1_infer(row_id, colno, confidence):
+    with analyzed_bayesdb_table(t1(), 1, 1) as (bdb, table_id):
+        bayeslite.bayesdb_infer(bdb, table_id, colno, row_id, None, confidence,
+            numsamples=1)
