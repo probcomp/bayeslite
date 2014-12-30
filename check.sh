b4bac6c924eb136e8e7a85991679ddcb45118611
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2014-12-30 17:48:55 +0000

    Add self-test script.

---
+++ check.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -Ceu
+
+: ${PYTHON:=python}
+: ${PY_TEST:=py.test}
+
+root=`cd -- "$(dirname -- "$0")" && pwd`
+platform=`"${PYTHON}" -c 'import distutils.util as u; print u.get_platform()'`
+version=`"${PYTHON}" -c 'import sys; print sys.version[0:3]'`
+
+export PYTHONPATH="${root}/build/lib.${platform}-${version}"
+
+(
+    set -Ceu
+    echo "$PYTHONPATH"
+    cd -- "${root}"
+    "$PYTHON" setup.py build
+    cd tests
+    "$PY_TEST"
+)
