7d4cc3fc2a623b2154c2efcdda0890e15e6db85b
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2014-12-30 18:08:05 +0000

    Generate bayeslite/grammar.py automatically in setup.py.
    Mechanism is a mega-kludge because distutils and setuptools are
    brain-damaged.

--- setup.py
+++ setup.py
@@ -19,6 +19,40 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# XXX This is a mega-kludge.  Since distutils/setuptools has no way to
+# order dependencies (what kind of brain-dead build system can't do
+# this?), we just always regenerate the grammar.  Could hack
+# distutils.command.build to include a dependency mechanism, but this
+# is more expedient for now.
+grammars = [
+    'bayeslite/grammar.y',
+]
+
+import distutils.spawn
+import errno
+import os
+import os.path
+root = os.path.dirname(os.path.abspath(__file__))
+lemonade = root + '/external/lemonade/dist'
+for grammar in grammars:
+    parser = os.path.splitext(grammar)[0] + '.py'
+    parser_mtime = None
+    try:
+        parser_mtime = os.path.getmtime(parser)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+    if parser_mtime is not None:
+        if os.path.getmtime(grammar) < parser_mtime:
+            continue
+    print 'generating %s -> %s' % (grammar, parser)
+    distutils.spawn.spawn([
+        '/usr/bin/env', 'PYTHONPATH=' + lemonade,
+        lemonade + '/bin/lemonade',
+        '-s',                   # Write statistics to stdout.
+        grammar,
+    ])
+
 setup(
     name='bayeslite',
     version='0.0.dev',
