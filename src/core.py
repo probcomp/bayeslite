f2ffe183836b79138c1d51f132a0e683751ab843
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-04-29 22:08:07 +0000

    Docstrings for bayeslite.core.

--- src/core.py
+++ src/core.py
@@ -14,9 +14,46 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+"""Miscellaneous utilities for managing BayesDB entities.
+
+Tables, generators, and columns are named with strs.  Only US-ASCII is
+allowed, no Unicode.
+
+Each table has a nonempty sequence of named columns.  As in sqlite3,
+tables may be renamed and do not necessarily have numeric ids, so
+there is no way to have a handle on a table that is persistent outside
+a savepoint.
+
+Each table may optionally be modelled by any number of generators,
+representing a parametrized generative model for the table's data,
+according to a named metamodel.  For each table, at most one generator
+may be designated as the default generator for the table.
+
+Each generator models a subset of the columns in its table, which are
+called the modelled columns of that generator.  Each column in a
+generator has an associated statistical type.  Like tables, generators
+may be renamed.  Unlike tables, each generator has a numeric id, which
+is never reused and therefore persistent across savepoints.
+
+Each generator may have any number of different models, each
+representing a particular choice of parameters for the parametrized
+generative model.  Models are numbered consecutively for the
+generator, and may be identified uniquely by (generator_id, modelno)
+or (generator_name, modelno).
+
+Tables and generators may not share names.  In most contexts, where a
+generator's name is needed, the name of a table with a default
+generator may be substituted.
+
+"""
+
 from bayeslite.sqlite3_util import sqlite3_quote_name
 
 def bayesdb_has_table(bdb, name):
+    """True if there is a table named `name` in `bdb`.
+
+    The table need not be modelled.
+    """
     qt = sqlite3_quote_name(name)
     cursor = bdb.sql_execute('PRAGMA table_info(%s)' % (qt,))
     try:
@@ -27,6 +64,13 @@ def bayesdb_has_table(bdb, name):
         return True
 
 def bayesdb_table_column_names(bdb, table):
+    """Return a list of names of columns in the table named `table`.
+
+    The results strs and are ordered by column number.
+
+    `bdb` must have a table named `table`.  If you're not sure, call
+    :func:`bayesdb_has_table` first.
+    """
     if not bayesdb_has_table(bdb, table):
         raise ValueError('No such table: %s' % (repr(table),))
     sql = '''
@@ -37,6 +81,11 @@ def bayesdb_table_column_names(bdb, table):
     return [str(row[0]) for row in bdb.sql_execute(sql, (table,))]
 
 def bayesdb_table_column_name(bdb, table, colno):
+    """Return the name of the column numbered `colno` in `table`.
+
+    `bdb` must have a table named `table`.  If you're not sure, call
+    :func:`bayesdb_has_table` first.
+    """
     sql = '''
         SELECT name FROM bayesdb_column WHERE tabname = ? AND colno = ?
     '''
@@ -50,6 +99,11 @@ def bayesdb_table_column_name(bdb, table, colno):
         return row[0]
 
 def bayesdb_table_column_number(bdb, table, name):
+    """Return the number of column named `name` in `table`.
+
+    `bdb` must have a table named `table`.  If you're not sure, call
+    :func:`bayesdb_has_table` first.
+    """
     sql = '''
         SELECT colno FROM bayesdb_column WHERE tabname = ? AND name = ?
     '''
@@ -63,6 +117,11 @@ def bayesdb_table_column_number(bdb, table, name):
         return row[0]
 
 def bayesdb_table_guarantee_columns(bdb, table):
+    """Make sure ``bayesdb_column`` is populated with columns of `table`.
+
+    `bdb` must have a table named `table`.  If you're not sure, call
+    :func:`bayesdb_has_table` first.
+    """
     with bdb.savepoint():
         qt = sqlite3_quote_name(table)
         insert_column_sql = '''
@@ -78,16 +137,26 @@ def bayesdb_table_guarantee_columns(bdb, table):
             raise ValueError('No such table: %s' % (repr(table),))
 
 def bayesdb_table_has_column(bdb, table, name):
+    """True if the table named `table` has a column named `name`.
+
+    `bdb` must have a table named `table`.  If you're not sure, call
+    :func:`bayesdb_has_table` first.
+    """
     sql = 'SELECT COUNT(*) FROM bayesdb_column WHERE tabname = ? AND name = ?'
     cursor = bdb.sql_execute(sql, (table, name))
     return cursor.next()[0]
 
 def bayesdb_has_generator(bdb, name):
+    """True if there is a generator named `name` in `bdb`.
+
+    Only actual generator names are considered.
+    """
     sql = 'SELECT COUNT(*) FROM bayesdb_generator WHERE name = ?'
     cursor = bdb.sql_execute(sql, (name,))
     return cursor.next()[0] != 0
 
 def bayesdb_has_generator_default(bdb, name):
+    """True if there is a generator or default-modelled table named `name`."""
     sql = '''
         SELECT COUNT(*) FROM bayesdb_generator
             WHERE name = :name OR (defaultp AND tabname = :name)
@@ -96,6 +165,14 @@ def bayesdb_has_generator_default(bdb, name):
     return cursor.next()[0] != 0
 
 def bayesdb_get_generator(bdb, name):
+    """Return the id of the generator named `name` in `bdb`.
+
+    The id is persistent across savepoints: ids are 64-bit integers
+    that increase monotonically and are never reused.
+
+    `bdb` must have a generator named `name`.  If you're not sure,
+    call :func:`bayesdb_has_generator` first.
+    """
     sql = 'SELECT id FROM bayesdb_generator WHERE name = ?'
     cursor = bdb.sql_execute(sql, (name,))
     try:
@@ -107,6 +184,15 @@ def bayesdb_get_generator(bdb, name):
         return row[0]
 
 def bayesdb_get_generator_default(bdb, name):
+    """Return the id of the (default) generator named `name` in `bdb`.
+
+    The id is persistent across savepoints: ids are 64-bit integers
+    that increase monotonically and are never reused.
+
+    `bdb` must have a generator named `name`, or a modelled table
+    named `name` with a default generator.  If you're not sure, call
+    :func:`bayesdb_has_generator_default` first.
+    """
     sql = '''
         SELECT id FROM bayesdb_generator
             WHERE name = :name OR (defaultp AND tabname = :name)
@@ -121,6 +207,7 @@ def bayesdb_get_generator_default(bdb, name):
         return row[0]
 
 def bayesdb_generator_name(bdb, id):
+    """Return the name of the generator with id `id`."""
     sql = 'SELECT name FROM bayesdb_generator WHERE id = ?'
     cursor = bdb.sql_execute(sql, (id,))
     try:
@@ -131,6 +218,7 @@ def bayesdb_generator_name(bdb, id):
         return row[0]
 
 def bayesdb_generator_metamodel(bdb, id):
+    """Return the name of the metamodel of the generator with id `id'."""
     sql = 'SELECT metamodel FROM bayesdb_generator WHERE id = ?'
     cursor = bdb.sql_execute(sql, (id,))
     try:
@@ -145,6 +233,7 @@ def bayesdb_generator_metamodel(bdb, id):
         return bdb.metamodels[row[0]]
 
 def bayesdb_generator_table(bdb, id):
+    """Return the name of the table of the generator with id `id'."""
     sql = 'SELECT tabname FROM bayesdb_generator WHERE id = ?'
     cursor = bdb.sql_execute(sql, (id,))
     try:
@@ -156,6 +245,7 @@ def bayesdb_generator_table(bdb, id):
         return row[0]
 
 def bayesdb_generator_column_names(bdb, generator_id):
+    """Return a list of names of columns modelled by `generator_id`."""
     sql = '''
         SELECT c.name
             FROM bayesdb_column AS c,
@@ -171,6 +261,7 @@ def bayesdb_generator_column_names(bdb, generator_id):
     return [str(row[0]) for row in bdb.sql_execute(sql, (generator_id,))]
 
 def bayesdb_generator_column_stattype(bdb, generator_id, colno):
+    """Return the statistical type of the column `colno` in `generator_id`."""
     sql = '''
         SELECT stattype FROM bayesdb_generator_column
             WHERE generator_id = ? AND colno = ?
@@ -202,6 +293,7 @@ def bayesdb_generator_column_stattype(bdb, generator_id, colno):
         return row[0]
 
 def bayesdb_generator_column_name(bdb, generator_id, colno):
+    """Return the name of the column numbered `colno` in `generator_id`."""
     sql = '''
         SELECT c.name
             FROM bayesdb_generator AS g,
@@ -228,6 +320,7 @@ def bayesdb_generator_column_name(bdb, generator_id, colno):
         return row[0]
 
 def bayesdb_generator_column_number(bdb, generator_id, column_name):
+    """Return the number of the column `column_name` in `generator_id`."""
     sql = '''
         SELECT c.colno
             FROM bayesdb_generator AS g,
@@ -254,6 +347,7 @@ def bayesdb_generator_column_number(bdb, generator_id, column_name):
         return row[0]
 
 def bayesdb_generator_column_numbers(bdb, generator_id):
+    """Return a list of the numbers of columns modelled in `generator_id`."""
     sql = '''
         SELECT colno FROM bayesdb_generator_column
             WHERE generator_id = ?
@@ -262,6 +356,7 @@ def bayesdb_generator_column_numbers(bdb, generator_id):
     return [row[0] for row in bdb.sql_execute(sql, (generator_id,))]
 
 def bayesdb_generator_has_model(bdb, generator_id, modelno):
+    """True if `generator_id` has a model numbered `modelno`."""
     sql = '''
         SELECT COUNT(*) FROM bayesdb_generator_model AS m
             WHERE generator_id = ? AND modelno = ?
