014f23d91af1ca985f7513f0c74927d87b53ed3b
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-04-29 00:12:35 +0000

    First draft of docstring for top-level bayeslite module.

--- src/__init__.py
+++ src/__init__.py
@@ -14,7 +14,43 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# Public API.
+"""Probabilistic database built on SQLite 3.
+
+Bayeslite is a probabilistic database built on `SQLite 3
+<https://www.sqlite.org/>`__.  In addition to SQL queries on
+conventional SQL tables, it supports probabilistic BQL queries on
+generative models for data in a table.
+
+The focus of the bayeslite API is the *BayesDB*, a handle for a
+database in memory or on disk.  To obtain a BayesDB handle, either
+creating one in memory or opening or creating one on disk, use
+:func:`bayesdb_open`::
+
+   import bayeslite
+
+   memdb = bayeslite.bayesdb_open()
+   filedb = bayeslite.bayesdb_open(pathname='foo.bdb')
+
+When done, close it with the :meth:`~BayesDB.close` method::
+
+   filedb.close()
+   memdb.close()
+
+The contents of ``memdb`` will be forgotten when it is closed or when
+the Python process exists.  The contents of ``filedb`` will be stored
+durably on disk in ``foo.bdb``.  The BayesDB object acts as a context
+manager that returns itself on entry and closes itself on exit, so you
+can write::
+
+   with bayeslite.bayesdb_open(pathname='foo.bdb') as bdb:
+       ...
+
+Given a BayesDB handle ``bdb``, execute BQL queries on it with the
+:meth:`~BayesDB.execute` method::
+
+   bdb.execute('create table t(x int, y text, z real)')
+"""
+
 from bayeslite.bayesdb import BayesDB
 from bayeslite.bayesdb import bayesdb_open
 from bayeslite.bqlfn import bayesdb_simulate
