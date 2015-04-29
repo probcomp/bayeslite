b7995ff5adc2c6fdb9e79a2b3623a7f8cdfaf0df
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-04-29 20:40:00 +0000

    Module docstring for legacy_models.py.

--- src/legacy_models.py
+++ src/legacy_models.py
@@ -14,6 +14,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+"""Support for legacy models from the previous incarnation of BayesDB."""
+
 import gzip
 import json
 import pickle
