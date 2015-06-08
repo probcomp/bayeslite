fadc4e1b4af33edff3923d622997b9029d9fc4dd
F Saad <fsaad@mit.edu>
2015-06-08 18:59:24 -0400

    explore continous hyperparams by iteration

--- experiments/exp_hyper_cont_iter.py
+++ experiments/exp_hyper_cont_iter.py
@@ -4,7 +4,8 @@ import exputils as eu
 
 import numpy as np
 import pandas as pd
-
+import scipy.stats as stats
+from scipy.special import gammaln
 
 from crosscat.MultiprocessingEngine import MultiprocessingEngine
 from crosscat.tests.quality_tests import synthetic_data_generator as sdg
@@ -15,17 +16,18 @@ import sys
 import tempfile
 import json
 
+
 DO_PLOT = True
 try:
     from matplotlib import pyplot as plt
     from matplotlib import gridspec
     try:
         import seaborn as sns
+        plt.rc('font', weight='bold')
     except:
         pass
 except ImportError:
     DO_PLOT = False
-
 
 def pprint(cursor):
     return pp_cursor(sys.stdout, cursor)
@@ -44,8 +46,7 @@ def train_models(args):
     model_hypers = {i:[] for i in xrange(args['n_model'])}
     for k in xrange(args['n_model']):
         
-        print "ANALYZING MODEL {}: Iterations".format(k),
-        sys.stdout.flush()
+        print "ANALYZING MODEL {} for {} Iterations".format(k, args['target_iters'])
 
         temp = tempfile.NamedTemporaryFile()
         eu.data_to_csv(np.asarray(args['dataset']), temp.name)
@@ -68,7 +69,6 @@ def train_models(args):
                 {}
             );
         '''.format(generator, qt_btable, str(C)[2:-2].replace('\'',''))
-        print bql
         bdb.execute(bql)
 
         bql = '''
@@ -78,13 +78,8 @@ def train_models(args):
 
         total_iters = args['step_size']
         while (total_iters <= args['target_iters']):
-            print total_iters
+            print total_iters,
             sys.stdout.flush()
-
-            bql = '''
-            SELECT * FROM {};
-            '''.format(sqlite3_quote_name('bayesdb_crosscat_diagnostics'))
-            pprint(bdb.execute(bql))
         
             bql = '''
             ANALYZE {} FOR {} ITERATIONS WAIT;
@@ -122,33 +117,46 @@ def plot(result, filename=None):
     n_model = args['n_model']
     step_size = args['step_size']
     target_iters = args['target_iters']
+    actual_params = args['actual_component_params']
+    actual_weights = args['actual_component_weights']
 
     fig, ax = plt.subplots()
-    ax.set_xlabel('Number of Iterations')
-    ax.set_ylabel(r'Hyperparamter $\mu$ (Mean of Gaussian Mixture)')
-    ax.set_title('Mean of Posterior Mean with {} Samples'.format(args['n_samples']))
+    ax.set_xlabel('Number of Iterations', fontweight = 'bold')
+    ax.set_ylabel(r'$\log P(\mu,\rho | D)$', fontweight = 'bold')
+    ax.set_title(r'Log Density of Mixture Component Mean and Precison $(\mu,\rho)$'+'\n'+
+        'Under Posterior Dirichlet Process Base Distribution')
 
     xs = np.arange(step_size, target_iters + 1, step_size)
-    averages = [0]*len(xs)
+    model_log_pdfs = np.zeros((args['n_model'], len(actual_params), len(xs)))
     for model in xrange(args['n_model']):
-        (m,s,r,nu) = ([],[],[],[])
-        for (i,h) in enumerate(result['hypers'][model]):
-            m.append(h['mu'])
-            s.append(h['shell'])
-            r.append(h['r'])
-            nu.append(h['nu'])
-            averages[i] += h['mu'] / args['n_model']
-        ax.plot(xs, m, alpha = 0.4)
-    ax.plot(xs, averages, alpha = 1, color = 'black', linestyle = '--', label = 'CC Mean')
-    ax.text(ax.get_xlim()[1], averages[-1], r'$\mu_{{CC}} = {:.3f}$'.format(averages[-1]), color = 'black')
+        for (id_h, hypers) in enumerate(actual_params):
+            for (id_iter, h) in enumerate(result['hypers'][model]):
+                m, s, r, nu = h['mu'], h['s'], h['r'], h['nu']
+                (mu, rho) = hypers['mu'], hypers['rho']
+                log_pdf_gamma = stats.gamma.logpdf(rho,a=nu/2.,scale=2./s)
+                log_pdf_normal = stats.norm.logpdf(mu,loc=m,scale = np.sqrt(1./(r*rho)))
+                log_pdf_joint = log_pdf_gamma + log_pdf_normal
+                model_log_pdfs[model, id_h, id_iter] = log_pdf_joint
+    log_pdf_means = np.mean(model_log_pdfs, axis = 0)
+    for model_array in model_log_pdfs:
+        for (i, row) in enumerate(model_array):
+            ax.plot(xs, row, alpha = 0.1, color = 'rbgmcyk'[i])    
+    for (i,row) in enumerate(log_pdf_means):
+        ax.plot(xs, row, label = r'$w_{}={}$'.format(i, args['actual_component_weights'][i]), color = 'rbgmcyk'[i])
+    
+    ax.legend(loc=3)
+    ax.grid()
 
-    actual_average = 0
-    for i,(z,w) in enumerate(zip(args['actual_component_params'],args['actual_component_weights'])):
-        ax.axhline(y = z['mu'], color = 'blue', alpha = 0.4)
-        ax.text(ax.get_xlim()[1], z['mu'], r'$(w_{},\mu_{})=({:.2f},{:.2f})$'.format(i,i,w,z['mu']), color = 'blue')
-        actual_average += z['mu'] * w
-    ax.axhline(y = actual_average, color = 'red', linestyle='--')
-    ax.text(ax.get_xlim()[1], actual_average, r'$\sum w_kp_k={:.3f}$'.format(actual_average), color = 'red')
+    # ax.plot(xs, averages, alpha = 1, color = 'black', linestyle = '--', label = 'CC Mean')
+    # ax.text(ax.get_xlim()[1], averages[-1], r'$\mu_{{CC}} = {:.3f}$'.format(averages[-1]), color = 'black')
+
+    # actual_average = 0
+    # for i,(z,w) in enumerate(zip(args['actual_component_params'],args['actual_component_weights'])):
+    #     ax.axhline(y = z['mu'], color = 'blue', alpha = 0.4)
+    #     ax.text(ax.get_xlim()[1], z['mu'], r'$(w_{},\mu_{})=({:.2f},{:.2f})$'.format(i,i,w,z['mu']), color = 'blue')
+    #     actual_average += z['mu'] * w
+    # ax.axhline(y = actual_average, color = 'red', linestyle='--')
+    # ax.text(ax.get_xlim()[1], actual_average, r'$\sum w_kp_k={:.3f}$'.format(actual_average), color = 'red')
 
     if not DO_PLOT:
         import time
@@ -163,19 +171,19 @@ if __name__ == '__main__':
     args = {
     'n_model' : 10,
     'step_size' : 5,
-    'target_iters' : 200,
+    'target_iters' : 50,
     'seed' : 448
     }
 
     # GENERATE SYNTHETIC DATA
-    cctypes = ['continu ous','continuous','multinomial', 'continuous', 'multinomial','multinomial',
+    cctypes = ['continuous','continuous','multinomial', 'continuous', 'multinomial','multinomial',
     'continuous','continuous','multinomial', 'continuous', 'continuous','continuous']
     distargs = [None, None, dict(K=9), None, dict(K=7), dict(K=4),
     None, None, dict(K=9), None, None, None]
     cols_to_views = [0, 0, 0, 1, 1, 2, 1, 0, 2, 3, 1, 0]
     cluster_weights = [[.3, .3, .4],[.6, .2, .1, .1],[.4, .4, .2],[.8, .2]]
     separation = [0.6, 0.4, 0.5, 0.6]
-    sdata = sdg.gen_data(cctypes, 
+    sdata = sdg.gen_data(cctypes,
         1000,
         cols_to_views, 
         cluster_weights, 
@@ -190,8 +198,8 @@ if __name__ == '__main__':
     args['actual_component_params'] = sdata[2]['component_params'][args['colno']]
     args['actual_component_weights'] = cluster_weights[cols_to_views[args['colno']]]
 
-    for samples in [10, 20, 50, 70, 100, 150, 200, 250, 300, 500]
-        args['n_samples'] = samples
-        args['dataset'] = np.asarray(sdata[0][:samples])
+    for n_samples in [20]:
+        args['n_samples'] = n_samples
+        args['dataset'] = np.asarray(sdata[0][:n_samples])
         result = runner(args)
         plot(result)
