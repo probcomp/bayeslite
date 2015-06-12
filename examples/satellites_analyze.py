09ecc1200f5bbf9076b88032a0f10919700d24ba
Anthony Lu <lua@mit.edu>
2015-06-12 15:58:47 -0400

    Fix satellites example to not crash

--- examples/satellites_analyze.py
+++ examples/satellites_analyze.py
@@ -1,7 +1,9 @@
 assert __name__ == '__main__'
 
+import bayeslite
 import bayeslite.bql as bql
 import bayeslite.core as core
+import bayeslite.crosscat
 import bayeslite.parse as parse
 import crosscat.LocalEngine as localengine
 import getopt
@@ -31,10 +33,15 @@ for o, a in opts:
     else:
         assert False, 'bad option %s' % (o,)
 
-bdb = core.BayesDB(localengine.LocalEngine(seed=0), pathname='satellites.bdb')
-bdb.execute("create btable if not exists satellites" +
-    " from 'satellites.utf8.csv'")
-bdb.execute('initialize 10 models if not exists for satellites')
+bdb = bayeslite.bayesdb_open(pathname='satellites.bdb')
+bayeslite.bayesdb_register_metamodel(bdb, bayeslite.crosscat.CrosscatMetamodel(localengine.LocalEngine(seed=0)))
+try:
+    bayeslite.bayesdb_read_csv_file(bdb, 'satellites', 'satellites.csv', header=True, create=True)
+except ValueError: # table exists
+    pass
+else:
+    bdb.execute('create default generator satellites_cc for satellites using crosscat (guess(*))')
+    bdb.execute('initialize 10 models for satellites')
 if iterations is not None:
     modelspec = 'models %s' % (modelnos,) if modelnos is not None else ''
     bdb.execute('analyze satellites %s for %d iterations wait' %
