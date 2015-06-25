f093129bb0d1f70c5b7bcae034ae8c0b4cb7641d
F Saad <fsaad@mit.edu>
2015-06-25 14:23:57 -0400

    added test for codebook value map

---
+++ tests/test_codebook_valmap.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2014, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import os
+import exceptions
+import pytest
+
+import crosscat.LocalEngine
+
+import bayeslite
+import bayeslite.crosscat
+from bayeslite.sqlite3_util import sqlite3_quote_name as quote
+
+root = os.path.dirname(os.path.abspath(__file__))
+DUMMY_CODEBOOK = os.path.join(root, 'dummy_codebook.csv')
+DUMMY_DATA = os.path.join(root, 'dummy.csv')
+
+def test_codebook_valmap():
+    with bayeslite.bayesdb_open() as bdb:
+        cc = crosscat.LocalEngine.LocalEngine(seed=0)
+        ccme = bayeslite.crosscat.CrosscatMetamodel(cc)
+        bayeslite.bayesdb_register_metamodel(bdb, ccme)
+        
+        bayeslite.bayesdb_read_csv(bdb, 'dummy', file(DUMMY_DATA),
+            header = True,
+            create = True)
+        
+        bayeslite.bayesdb_load_codebook_csv_file(bdb, 'dummy', DUMMY_CODEBOOK)
+
+        bdb.execute("""
+        CREATE GENERATOR dummy_cc FOR dummy 
+            USING crosscat(
+                GUESS(*),
+                kerberos IGNORE,
+                age NUMERICAL,
+                city CATEGORICAL
+            );
+        """)
+
+        bdb.execute("""
+        INITIALIZE 10 MODELS FOR dummy_cc;
+        """)
+
+        bdb.execute("""
+        ANALYZE dummy_cc FOR 20 ITERATIONS WAIT;
+        """)
+
+        bdb.execute("""
+        SIMULATE age FROM dummy_cc GIVEN city = RIO LIMIT 5 
+        """)
+
+        bdb.sql_execute("""
+        INSERT INTO dummy (kerberos, age, city) VALUES
+            ('jackie', 18, 'LA'), ('rocker', 22, 'DC');
+        """)
+
+        bdb.execute("""
+        ANALYZE dummy_cc FOR 20 ITERATIONS WAIT;
+        """)
+
+        with pytest.raises(exceptions.KeyError):
+            bdb.execute("""
+            SIMULATE age FROM dummy_cc GIVEN city = LA LIMIT 5;
+            """)
