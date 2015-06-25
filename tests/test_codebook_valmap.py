24547d24fb514582e3a3ebb7384f0f9c8bf055be
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-06-25 18:56:12 +0000

    Sort imports.

--- tests/test_codebook_valmap.py
+++ tests/test_codebook_valmap.py
@@ -13,8 +13,9 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import os
+
 import exceptions
+import os
 import pytest
 
 import crosscat.LocalEngine
