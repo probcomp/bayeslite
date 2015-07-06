45b274e9f2199c4a04487d9743ae1eb07df4c45e
F Saad <fsaad@mit.edu>
2015-07-06 18:20:48 -0400

    linted stats.py

--- src/stats.py
+++ src/stats.py
@@ -68,9 +68,9 @@ def pearsonr(a0, a1):
 
 def signum(x):
     """Computes the sign of `x`.
-    
+
     :param float x: Argument to signum.
-    
+
     :return: Sign of `x`: (``-1 if x<0, 0 if x=0, +1 if x>0``).
     :rtype: int
     """
@@ -82,23 +82,21 @@ def signum(x):
         return 0
 
 
-def chi2_contingency(contingency, correction=None):
-    """Computes observed Pearson Chi2 test statistic for a test of independence 
+def chi2_contingency(contingency, correction=True):
+    """Computes observed Pearson Chi2 test statistic for a test of independence
     on a contingency table.
     http://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test#Test_of_independence
 
     :param list<list> contingency: 2D table of observed frequencies. The
-    dimensions must be M by N, where M (resp N) is the number of discrete 
+    dimensions must be M by N, where M (resp N) is the number of discrete
     values taken by the first (resp second) random variable.
-    
+
     :param boolean correction: If ``True``, moves each observation count in the
     direction of the expectation by 1/2.
- 
+
     :return: The observed Pearson chi2 test statistic on the `contingency` table.
     :rtype: float
     """
-    if correction is None:
-        correction is True
     assert 0 < len(contingency)
     assert all(all(isinstance(v, int) for v in row) for row in contingency)
     n = float(sum(sum(row) for row in contingency))
@@ -123,9 +121,9 @@ def f_oneway(groups):
     (ANOVA).
     https://en.wikipedia.org/wiki/F-test#Multiple-comparison_ANOVA_problems
 
-    :param list<list> groups: List of lists of the observed values of each 
+    :param list<list> groups: List of lists of the observed values of each
     group. The outer list must length equal to the number of groups.
-    
+
     :return: The observed F test statistic on `groups`.
     :rtype: float
     """
@@ -150,39 +148,39 @@ def t_cdf(x, df):
     """Approximate cumulative distribution function for Student's t probability
     distribution.
     ``t_cdf(x,df) = P(T_df < x)``
-    Values are tested to within 0.5% of values returned by the 
+    Values are tested to within 0.5% of values returned by the
     Cephes C library for numerical integration.
 
     :param float x: Argument to the survival function, must be positive.
     :param float df: Degrees of freedom of the chi2 distribution.
- 
+
     :return: The area from negative infinity to `x` under the t probability
         distribution with degrees of freedom `df`.
     :rtype: float
     """
     import numpy
     numpy.random.seed(seed=0)
-    
+
     if df <= 0:
         raise ValueError("Degrees of freedom must be positive.")
     if x == 0:
         return 0.5
-    
+
     MONTE_CARLO_SAMPLES = 1e5
-    T = numpy.random.standard_t(df, size = MONTE_CARLO_SAMPLES)
+    T = numpy.random.standard_t(df, size=MONTE_CARLO_SAMPLES)
     p = numpy.sum(T < x) / MONTE_CARLO_SAMPLES
     return p
 
 def chi2_sf(x, df):
-    """Approximate survival function (tail) for the chi2 probability 
+    """Approximate survival function (tail) for the chi2 probability
     distribution.
     ``chi2_sf(x, df) = P(CHI > x)``
-    Values are tested to within 0.5% of values returned by the 
+    Values are tested to within 0.5% of values returned by the
     Cephes C library for numerical integration.
 
     :param float x: Argument to the survival function, must be positive.
     :param float df: Degrees of freedom of the chi2 distribution.
- 
+
     :return: The area from `x` to infinity under the chi2 probability
         distribution with degrees of freedom df.
     :rtype: float
@@ -194,7 +192,7 @@ def chi2_sf(x, df):
         raise ValueError("Degrees of freedom must be positive.")
     if x <= 0:
         return 1.0
-    
+
     MONTE_CARLO_SAMPLES = 5e5
     CHI = numpy.random.chisquare(df, size = MONTE_CARLO_SAMPLES)
     p = numpy.sum(CHI > x) / MONTE_CARLO_SAMPLES
@@ -210,7 +208,7 @@ def f_sf(x, df_num, df_den):
     :param float x: Argument to the survival function, must be positive.
     :param float df_num: Degrees of freedom of the numerator.
     :param float df_den: Degrees of freedom of the denominator.
- 
+
     :return: The area from negative infinity to `x` under the t probability
         distribution with degrees of freedom `df`.
     :rtype: float
@@ -222,7 +220,7 @@ def f_sf(x, df_num, df_den):
         raise ValueError("Degrees of freedom must be positive.")
     if x <= 0:
         return 1.0
-    
+
     MONTE_CARLO_SAMPLES = 1e5
     F = numpy.random.f(df_num, df_den, size = MONTE_CARLO_SAMPLES)
     p = numpy.sum(F > x) / MONTE_CARLO_SAMPLES
