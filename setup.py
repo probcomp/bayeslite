baff1356d11f4b6ea206a0938b15e428d7490dc9
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-07-24 20:38:54 +0000

    v0.1.dev20150724

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-version = '0.1.dev'
+version = '0.1.dev20150724'
 
 try:
     from setuptools import setup
