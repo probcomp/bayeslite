b4016e28595e6269b97042c2891a9d6a4e943f14
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-07-31 00:08:08 +0000

    Remove debugging message now that autobuild is fixed.
    (Still not entirely sure why `git describe --always' broke it, but
    whatever...)

--- setup.py
+++ setup.py
@@ -33,8 +33,6 @@ if version.endswith('+'):
     except Exception:
         version += 'unknown'
     else:
-        print '# tag: %s' % (tag,)
-        print '# git describe: %s' % (desc,)
         assert desc.startswith(tag)
         version = desc[1:].strip()
 
