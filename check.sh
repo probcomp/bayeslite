6411a70538912db657179fe8adcb1ba3d4786195
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-09-15 20:27:03 +0000

    Disable tracking bug in check.sh.

--- check.sh
+++ check.sh
@@ -17,9 +17,11 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
     cd -- "${root}"
     rm -rf build
     ./pythenv.sh "$PYTHON" setup.py build
+    export BAYESDB_WIZARD_MODE=1
+    export BAYESDB_DISABLE_VERSION_CHECK=1
     if [ $# -eq 0 ]; then
-        BAYESDB_WIZARD_MODE=1 ./pythenv.sh "$PYTHON" "$PY_TEST" tests shell/tests
+        ./pythenv.sh "$PYTHON" "$PY_TEST" tests shell/tests
     else
-        BAYESDB_WIZARD_MODE=1 ./pythenv.sh "$PYTHON" "$PY_TEST" "$@"
+        ./pythenv.sh "$PYTHON" "$PY_TEST" "$@"
     fi
 )
