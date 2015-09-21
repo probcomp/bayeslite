c147cf1609c71cb5b8c3302f97b7c26525265777
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-09-21 13:40:13 +0000

    Don't need pythenv.sh for `python setup.py build'.

--- check.sh
+++ check.sh
@@ -16,7 +16,7 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
     set -Ceu
     cd -- "${root}"
     rm -rf build
-    ./pythenv.sh "$PYTHON" setup.py build
+    "$PYTHON" setup.py build
     export BAYESDB_WIZARD_MODE=1
     export BAYESDB_DISABLE_VERSION_CHECK=1
     if [ $# -eq 0 ]; then
