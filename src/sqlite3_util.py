76781522b6a5cca5b0d1fd1d01b9153ccd8343b7
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2015-11-19 21:06:25 +0000

    Implement sqlite3_connection context manager: close on exit.

--- src/sqlite3_util.py
+++ src/sqlite3_util.py
@@ -19,6 +19,16 @@
 import binascii
 import contextlib
 import os
+import sqlite3
+
+@contextlib.contextmanager
+def sqlite3_connection(*args, **kwargs):
+    """SQLite3 connection context manager.  On exit, runs close."""
+    connection = sqlite3.connect(*args, isolation_level=None, **kwargs)
+    try:
+        yield connection
+    finally:
+        connection.close()
 
 @contextlib.contextmanager
 def sqlite3_transaction(db):
