be5000c0f9ce08a9a2beba7e80de8c5a24a8bef2
Gregory Marton <gremio@acm.org>
2016-02-08 15:36:28 -0500

    As @riastradh suggests, let pytest fail on its own.

--- check.sh
+++ check.sh
@@ -6,14 +6,6 @@ set -Ceu
 
 root=`cd -- "$(dirname -- "$0")" && pwd`
 
-function ensure_pytest() {
-    ok='This is pytest'
-    if ! ./pythenv.sh "$PYTHON" -m pytest --version 2>&1 | grep -q "$ok"; then
-        printf >&2 'ERROR: Unable to find pytest. Will not run tests.\n'
-        exit 1
-    fi
-}
-
 (
     set -Ceu
     cd -- "${root}"
@@ -21,7 +13,6 @@ function ensure_pytest() {
     "$PYTHON" setup.py build
     export BAYESDB_WIZARD_MODE=1
     export BAYESDB_DISABLE_VERSION_CHECK=1
-    ensure_pytest
     if [ $# -eq 0 ]; then
         # By default, when running all tests, skip tests that have
         # been marked for continuous integration by using __ci_ in
