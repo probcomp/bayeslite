0315fc59b1d831d74da460645e2adfd55370774c
Gregory Marton <gremio@acm.org>
2016-04-14 13:24:14 -0400

    Add LTS test. Interestingly, it's the same as the 15:10 test. Note that both are a bad idea to have here.

--- check.sh
+++ check.sh
@@ -21,8 +21,12 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
                      tests shell/tests
     elif [ "docker" = "$1" ]; then
         shift
+        echo "WARNING: Possible version skew, because using crosscat from pypi."
+        # XXX: Leave all docker-based testing to packaging, where we have info
+        #      about corresponding versions that we don't have here.
         python setup.py sdist
-        docker build -f tests/Dockerfile -t bayeslite-test "$@" .
+        docker build -f tests/Dockerfile -t bayeslite-test-15-10 "$@" .
+        docker build -f tests/LTS-Dockerfile -t bayeslite-test-14-04 "$@" .
     else
         # If args are specified, run all tests, including continuous
         # integration tests, for the selected components.
