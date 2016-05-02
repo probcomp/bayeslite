c144d65906b24eb01f42886e09f93fd6bce4446c
Gregory Marton <gremio@acm.org>
2016-05-02 06:32:57 -0400

    Updated crosscat

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ setup(
     license='Apache License, Version 2.0',
     install_requires=[
         'bayeslite-apsw>=3.8.0',
-        'crosscat==0.1.53',
+        'crosscat==0.1.54',
         'jsonschema',
         'numpy',
         'requests',
