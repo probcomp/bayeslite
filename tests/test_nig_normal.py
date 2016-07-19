31fdeeefe2de61ed6a4836e296373c40eac0ab20
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2016-07-19 19:15:22 +0000

    Test some more conditionals involving latents.

--- tests/test_nig_normal.py
+++ tests/test_nig_normal.py
@@ -127,6 +127,72 @@ def test_nig_normal_latent_smoke():
         bdb.execute('drop population p')
         bdb.execute('drop table t')
 
+def test_nig_normal_latent_conditional_smoke():
+    with bayesdb_open(':memory:') as bdb:
+        bayesdb_register_metamodel(bdb, NIGNormalMetamodel())
+        bdb.sql_execute('create table t(x)')
+        for x in xrange(100):
+            bdb.sql_execute('insert into t(x) values(?)', (x,))
+        bdb.execute('create population p for t(x numerical)')
+        bdb.execute('create generator g0 for p using nig_normal')
+        bdb.execute('''
+            create generator g1 for p using nig_normal(xe deviation(x))
+        ''')
+        bdb.execute('initialize 1 model for g0')
+        bdb.execute('analyze g0 for 1 iteration wait')
+        bdb.execute('initialize 1 model for g1')
+        bdb.execute('analyze g1 for 1 iteration wait')
+
+        # observed given observed
+        bdb.execute('''
+            estimate probability of x = 50 given (x = 50) within p
+        ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (x = 50) within p
+                modelled by g0
+        ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (x = 50) within p
+                modelled by g1
+        ''').fetchall()
+
+        # observed given latent
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of x = 50 given (xe = 50) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of x = 50 given (xe = 50) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (xe = 50) within p
+                modelled by g1
+        ''').fetchall()
+
+        # latent given observed
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 50 given (x = 50) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 50 given (x = 50) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of xe = 50 given (x = 50) within p
+                modelled by g1
+        ''').fetchall()
+
+        bdb.execute('drop models from g0')
+        bdb.execute('drop generator g0')
+        bdb.execute('drop models from g1')
+        bdb.execute('drop generator g1')
+        bdb.execute('drop population p')
+        bdb.execute('drop table t')
+
 def test_nig_normal_latent_2var_smoke():
     with bayesdb_open(':memory:') as bdb:
         bayesdb_register_metamodel(bdb, NIGNormalMetamodel())
@@ -197,6 +263,153 @@ def test_nig_normal_latent_2var_smoke():
             assert 10 == len(bdb.execute('''
                 simulate xe from p modeled by g0 limit 10;
             ''').fetchall())
+
+        bdb.execute('drop models from g0')
+        bdb.execute('drop generator g0')
+        bdb.execute('drop models from g1')
+        bdb.execute('drop generator g1')
+        bdb.execute('drop population p')
+        bdb.execute('drop table t')
+
+def test_nig_normal_latent_2var_conditional_smoke():
+    with bayesdb_open(':memory:') as bdb:
+        bayesdb_register_metamodel(bdb, NIGNormalMetamodel())
+        bdb.sql_execute('create table t(x, y)')
+        for x in xrange(100):
+            bdb.sql_execute('insert into t(x, y) values(?, ?)',
+                (x, x*x - 100))
+        bdb.execute('create population p for t(x numerical, y numerical)')
+
+        # CORRELATION, CORRELATION PVALUE, without generators.
+        assert 4 == len(bdb.execute('''
+            estimate correlation, correlation pvalue
+                from pairwise variables of p
+        ''').fetchall())
+
+        bdb.execute('create generator g0 for p using nig_normal')
+        bdb.execute('''
+            create generator g1 for p using nig_normal(xe deviation(x))
+        ''')
+        bdb.execute('initialize 1 model for g0')
+        bdb.execute('analyze g0 for 1 iteration wait')
+        bdb.execute('initialize 1 model for g1')
+        bdb.execute('analyze g1 for 1 iteration wait')
+
+        # observed given other observed
+        bdb.execute('''
+            estimate probability of x = 50 given (y = 49) within p
+        ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (y = 49) within p
+                modelled by g0
+        ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (y = 49) within p
+                modelled by g1
+        ''').fetchall()
+        bdb.execute('simulate x from p given y = 49 limit 1').fetchall()
+        bdb.execute('''
+            simulate x from p modelled by g0 given y = 49 limit 1
+        ''').fetchall()
+        bdb.execute('''
+            simulate x from p modelled by g1 given y = 49 limit 1
+        ''').fetchall()
+
+        # observed given related latent
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of x = 50 given (xe = 1) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of x = 50 given (xe = 1) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of x = 50 given (xe = 1) within p
+                modelled by g1
+        ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('simulate x from p given xe = 1 limit 1').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                simulate x from p modelled by g0 given xe = 1 limit 1
+            ''').fetchall()
+        bdb.execute('''
+            simulate x from p modelled by g1 given xe = 1 limit 1
+        ''').fetchall()
+
+        # observed given unrelated latent
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of y = 50 given (xe = 1) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of y = 50 given (xe = 1) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of y = 50 given (xe = 1) within p
+                modelled by g1
+        ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('simulate y from p given xe = 1 limit 1').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                simulate y from p modelled by g0 given xe = 1 limit 1
+            ''').fetchall()
+        bdb.execute('''
+            simulate y from p modelled by g1 given xe = 1 limit 1
+        ''').fetchall()
+
+        # latent given related observed
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 1 given (x = 50) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 1 given (x = 50) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of xe = 1 given (x = 50) within p
+                modelled by g1
+        ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('simulate xe from p given x = 50 limit 1').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                simulate xe from p modelled by g0 given x = 50 limit 1
+            ''').fetchall()
+        bdb.execute('''
+            simulate xe from p modelled by g1 given x = 50 limit 1
+        ''').fetchall()
+
+        # latent given unrelated observed
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 1 given (y = 50) within p
+            ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                estimate probability of xe = 1 given (y = 50) within p
+                    modelled by g0
+            ''').fetchall()
+        bdb.execute('''
+            estimate probability of xe = 1 given (y = 50) within p
+                modelled by g1
+        ''').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('simulate xe from p given y = 50 limit 1').fetchall()
+        with pytest.raises(BQLError):
+            bdb.execute('''
+                simulate xe from p modelled by g0 given y = 50 limit 1
+            ''').fetchall()
+        bdb.execute('''
+            simulate xe from p modelled by g1 given y = 50 limit 1
+        ''').fetchall()
 
         bdb.execute('drop models from g0')
         bdb.execute('drop generator g0')
