9cfa7bc91ba612278d8c275df65ae3d45bcfe21d
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2016-10-27 00:56:18 +0000

    Add cgpm to install_requires.

--- setup.py
+++ setup.py
@@ -200,6 +200,7 @@ setup(
     license='Apache License, Version 2.0',
     install_requires=[
         'bayeslite-apsw>=3.8.0',
+        'cgpm',
         'crosscat>=0.1.55',
         'jsonschema',
         'numpy',
