27e21b9824512fb4697b95b2f2b65fb1b6fb45bd
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2017-02-24 22:24:23 +0000

    First draft of mutual information virtual table.
    Not hooked into anything yet.

---
+++ src/bqlvtab.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2016, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import apsw
+import json
+
+import bayeslite.bqlfn as bqlfn
+
+
+class Mutinf(object):
+    MI = 0
+    POPULATION_ID = 1
+    TARGET_VARS = 2
+    REFERENCE_VARS = 3
+    CONDITIONS = 4
+    NSAMPLES = 5
+
+
+class MutinfModule(object):
+
+    def __init__(self, bdb):
+        self._bdb = bdb
+
+    def Connect(self, connection, _modulename, _databasename, _tablename,
+            *_args):
+        schema = '''
+            create table t(
+                mi real not null,
+                population_id integer not null,
+                target_vars text not null,      -- json list
+                reference_vars text not null,   -- json list
+                conditions text,                -- json dict
+                nsamples integer
+            )
+        '''
+        table = MutinfTable(self._bdb)
+        return schema, table
+
+    Create = Connect
+
+
+class MutinfTable(object):
+
+    def __init__(self, bdb):
+        self._bdb = bdb
+
+    def Open(self):
+        return MutinfCursor(self._bdb)
+
+    def BestIndex(self, constraints, _orderbys):
+        # XXX Tidy this up and generalize this mechanism.
+        need = 0
+        need |= 1 << Mutinf.POPULATION_ID
+        need |= 1 << Mutinf.TARGET_VARS
+        need |= 1 << Mutinf.REFERENCE_VARS
+        have = 0
+        population_id = -1
+        target_vars = -1
+        reference_vars = -1
+        conditions = -1
+        nsamples = -1
+        for i, (c, op) in enumerate(constraints):
+            if op != apsw.SQLITE_INDEX_CONSTRAINT_EQ:
+                continue
+            if c == Mutinf.POPULATION_ID:
+                population_id = i
+            elif c == Mutinf.TARGET_VARS:
+                target_vars = i
+            elif c == Mutinf.REFERENCE_VARS:
+                reference_vars = i
+            elif c == Mutinf.CONDITIONS:
+                conditions = i
+            elif c == Mutinf.NSAMPLES:
+                nsamples = i
+            else:
+                continue
+            have |= 1 << c
+        if need & ~have:
+            # XXX Report clearer error message with names.
+            raise Exception('Missing constraints: %x' % (need & ~have,))
+        index_info = [None] * len(constraints)
+        count = Count()
+        index_info[population_id] = count.next()
+        index_info[target_vars] = count.next()
+        index_info[reference_vars] = count.next()
+        if have & (1 << Mutinf.CONDITIONS):
+            index_info[conditions] = count.next()
+        if have & (1 << Mutinf.NSAMPLES):
+            index_info[nsamples] = count.next()
+        return (index_info, have)
+
+
+class MutinfCursor(object):
+
+    def __init__(self, bdb):
+        self._bdb = bdb
+        self._rowid = None
+        self._mi = None
+        self._population_id = None
+        self._target_vars = None
+        self._reference_vars = None
+        self._conditions = None
+        self._nsamples = None
+
+    def Close(self):
+        pass
+
+    def Column(self, number):
+        return (
+            self._rowid,
+            self._mi[self._rowid],
+            self._population_id,
+            self._target_vars,
+            self._reference_vars,
+            self._conditions,
+            self._nsamples,
+        )[number + 1]
+
+    def Next(self):
+        self._rowid += 1
+
+    def Rowid(self):
+        return self._rowid
+
+    def Eof(self):
+        return not self._rowid < len(self._mi)
+
+    def Filter(self, indexnum, indexname, constraintargs):
+        assert indexnum & (1 << Mutinf.POPULATION_ID)
+        assert indexnum & (1 << Mutinf.TARGET_VARS)
+        assert indexnum & (1 << Mutinf.REFERENCE_VARS)
+        count = Count()
+        assert self._population_id is None
+        self._population_id = constraintargs[count.next()]
+        self._target_vars = constraintargs[count.next()]
+        self._reference_vars = constraintargs[count.next()]
+        if indexnum & (1 << Mutinf.CONDITIONS):
+            self._conditions = constraintargs[count.next()]
+        else:
+            self._conditions = None
+        if indexnum & (1 << Mutinf.NSAMPLES):
+            self._nsamples = constraintargs[count.next()]
+        else:
+            self._nsamples = None
+        self._rowid = 0
+        target_vars = json.loads(self._target_vars)
+        reference_vars = json.loads(self._reference_vars)
+        conditions_strkey = {} if self._conditions is None else \
+            json.loads(self._conditions)
+        conditions = \
+            {int(k): v for k, v in sorted(conditions_strkey.iteritems())}
+        # XXX Expose this API better from bqlfn.
+        mis = bqlfn._bql_column_mutual_information(
+            self._bdb, self._population_id, None,
+            target_vars, reference_vars, self._nsamples,
+            *_flatten2(conditions.iteritems()))
+        self._mi = _flatten2(mis)
+
+
+### Utilities
+
+def _flatten2(xss):
+    return [x for xs in xss for x in xs]
+
+class Count(object):
+    def __init__(self):
+        self._c = 0
+    def next(self):
+        c = self._c
+        self._c += 1
+        return c
---
+++ tests/test_vtab.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2016, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import test_core
+
+from stochastic import stochastic
+
+
+@stochastic(max_runs=2, min_passes=1)
+def test_mutinf_smoke(seed):
+    with test_core.t1(seed=seed) as (bdb, population_id, _generator_id):
+        def checkmi(n, q, *p):
+            i = 0
+            for r in bdb.sql_execute(q, *p):
+                assert len(r) == 1
+                assert isinstance(r[0], float)
+                i += 1
+            assert i == n, '%r =/= %r' % (i, n)
+
+        bdb.execute('initialize 10 models for p1_cc')
+        checkmi(10, '''
+            select mi from bql_mutinf
+                where population_id = ?
+                    and target_vars = '[1]'
+                    and reference_vars = '[2]'
+        ''', (population_id,))
+
+        bdb.execute('initialize 11 models if not exists for p1_cc')
+        checkmi(11, '''
+            select mi from bql_mutinf
+                where population_id = ?
+                    and target_vars = '[1]'
+                    and reference_vars = '[2]'
+                    and conditions = '{"3": 42}'
+        ''', (population_id,))
+
+        bdb.execute('initialize 12 models if not exists for p1_cc')
+        checkmi(12, '''
+            select mi from bql_mutinf
+                where population_id = ?
+                    and target_vars = '[1]'
+                    and reference_vars = '[2]'
+                    and nsamples = 2
+        ''', (population_id,))
+
+        bdb.execute('initialize 13 models if not exists for p1_cc')
+        checkmi(13, '''
+            select mi from bql_mutinf
+                where population_id = ?
+                    and target_vars = '[1]'
+                    and reference_vars = '[2]'
+                    and conditions = '{"3": 42}'
+                    and nsamples = 2
+        ''', (population_id,))
