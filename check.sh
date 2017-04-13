eae6f56103cc348bf69941b17583b68554847e5c
Taylor R Campbell <riastradh+probcomp@csail.mit.edu>
2017-04-13 18:54:35 +0000

    Remove vestigial docker case in check.sh.

--- check.sh
+++ check.sh
@@ -16,14 +16,6 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
         # been marked for continuous integration by using __ci_ in
         # their names.  (git grep __ci_ to find these.)
         ./pythenv.sh "$PYTHON" -m pytest --pyargs bayeslite -k "not __ci_"
-    elif [ "docker" = "$1" ]; then
-        shift
-        echo "WARNING: Possible version skew, because using crosscat from pypi."
-        # XXX: Leave all docker-based testing to packaging, where we have info
-        #      about corresponding versions that we don't have here.
-        python setup.py sdist
-        docker build -f tests/Dockerfile -t bayeslite-test-15-10 "$@" .
-        docker build -f tests/LTS-Dockerfile -t bayeslite-test-14-04 "$@" .
     else
         # If args are specified, run all tests, including continuous
         # integration tests, for the selected components.
