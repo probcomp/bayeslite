b0f1232a2176170d156ab9bcfd44f78fdc2a17aa
Michael Truell <michael_truell@horacemann.org>
2017-07-10 16:29:56 -0400

    Add logpdf_joint method

--- src/metamodels/loom_metamodel.py
+++ src/metamodels/loom_metamodel.py
@@ -3,11 +3,14 @@
 This module implements the :class:`bayeslite.IBayesDBMetamodel`
 interface for the Loom Model.
 """
+import collections
 import csv
+import gzip
 import json
 import os
 import os.path
 import tempfile
+
 
 import loom.tasks
 
@@ -24,13 +27,15 @@ INSERT INTO bayesdb_metamodel (name, version)
 CSV_DELIMITER = ','
 
 # TODO fill out
+# TODO optimize number of bdb calls
 STATTYPE_TO_LOOMTYPE = {'categorical': 'dd', 'numerical': 'nich'}
+
 
 class LoomMetamodel(metamodel.IBayesDBMetamodel):
     """Loom metamodel for BayesDB."""
 
-    def __init__(self,
-            data_path='/scratch/mntruell/venv/lib/python2.7/site-packages/data/'):
+    def __init__(self,data_path=
+            '/scratch/mntruell/venv/lib/python2.7/site-packages/data/'):
         self.DATA_PATH = data_path
 
     def name(self):
@@ -41,13 +46,6 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
             bdb.sql_execute(loom_schema, (self.name(),))
 
     def create_generator(self, bdb, generator_id, schema, **kwargs):
-        """
-        Converts the data encoded in the bdb table in question
-            into a temporary csv file
-        Ingests the data into the loom system
-        Stores the name of the loom session in a bdb table
-
-        """
         population_id = core.bayesdb_generator_population(bdb, generator_id)
         table = core.bayesdb_population_table(bdb, population_id)
 
@@ -83,9 +81,12 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
 
             # Prepare a schema json file
             json_dict = {}
-            for colno in core.bayesdb_variable_numbers(bdb, population_id, None):
-                column_name = core.bayesdb_variable_name(bdb, population_id, colno)
-                stattype = core.bayesdb_variable_stattype(bdb, population_id, colno)
+            for colno in core.bayesdb_variable_numbers(bdb,
+                    population_id, None):
+                column_name = core.bayesdb_variable_name(bdb,
+                        population_id, colno)
+                stattype = core.bayesdb_variable_stattype(bdb,
+                        population_id, colno)
                 json_dict[column_name] = STATTYPE_TO_LOOMTYPE[stattype]
 
             with tempfile.NamedTemporaryFile() as schema_file:
@@ -93,8 +94,9 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
                 schema_file.flush()
 
                 # Ingest data
-                loom.tasks.ingest(core.bayesdb_generator_name(bdb, generator_id),
-                        rows_csv=csv_file.name, schema=schema_file.name)
+                loom.tasks.ingest(
+                    core.bayesdb_generator_name(bdb, generator_id),
+                    rows_csv=csv_file.name, schema=schema_file.name)
 
                 # TODO store the string-int transformations - use in logpdf
 
@@ -102,18 +104,22 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
         return os.path.join(self.DATA_PATH, loom_name)
 
     def initialize_models(self, bdb, generator_id, modelnos):
-       self.modelnos = len(modelnos)
+        self.modelnos = len(modelnos)
 
     def analyze_models(self, bdb, generator_id, modelnos=None, iterations=1,
             max_seconds=None, ckpt_iterations=None, ckpt_seconds=None,
             program=None):
         name = core.bayesdb_generator_name(bdb, generator_id)
-        loom.tasks.infer(name, sample_count=(self.modelnos if modelnos is None else len(modelnos)))
+        loom.tasks.infer(name, sample_count = (
+            self.modelnos if modelnos is None else len(modelnos)))
 
-    def column_dependence_probability(self, bdb, generator_id, modelnos, colno0,
-            colno1):
-        server = loom.tasks.query(core.bayesdb_generator_name(bdb, generator_id))
-        output = server.relate([core.bayesdb_generator_column_name(bdb, generator_id, colno0),
+    def column_dependence_probability(self,
+            bdb, generator_id, modelnos, colno0, colno1):
+        # TODO cache
+        server = loom.tasks.query(
+                core.bayesdb_generator_name(bdb, generator_id))
+        output = server.relate(
+                [core.bayesdb_generator_column_name(bdb, generator_id, colno0),
             core.bayesdb_generator_column_name(bdb, generator_id, colno1)])
         split_array = [a.split(CSV_DELIMITER) for a in output.split('\n')]
 
@@ -133,21 +139,17 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
 
     def simulate_joint(self, bdb, generator_id, modelnos, rowid, targets,
             constraints, num_samples=1, accuracy=None):
-        # Prepare a predicted row
+
         headers = []
         row = []
         for colno in targets:
-            headers.append(core.bayesdb_generator_column_name(bdb, generator_id, colno))
-
-            is_constraint = False
-            for (colno2, value) in constraints:
-                if colno2 == colno:
-                    row.append(value)
-                    is_constraint = True
-                    break;
-
-            if is_constraint == False:
-                row.append('')
+            headers.append(core.bayesdb_generator_column_name(bdb,
+                generator_id, colno))
+            row.append('')
+        for (colno, value) in constraints:
+            headers.append(core.bayesdb_generator_column_name(bdb,
+                generator_id, colno))
+            row.append(value)
 
         # Write to csv
         with tempfile.NamedTemporaryFile() as temp_file:
@@ -157,11 +159,13 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
             temp_file.flush()
 
             # Simulate values
+            # TODO cache
             server = loom.tasks.query(core.bayesdb_generator_name(bdb, generator_id))
             output = server.predict(temp_file.name, num_samples, id_offset=False)
 
             # Parse output
-            loom_output = [a.split(CSV_DELIMITER) for a in output.strip().split('\r\n')[1:]]
+            loom_output = [a.split(CSV_DELIMITER)
+                    for a in output.strip().split('\r\n')[1:]]
 
             # Convert output values to proper data types
             population_id = core.bayesdb_generator_population(bdb, generator_id)
@@ -170,16 +174,79 @@ class LoomMetamodel(metamodel.IBayesDBMetamodel):
                     stattype = core.bayesdb_variable_stattype(
                             bdb, population_id, targets[col_index])
                     # TODO dont use private
-                    if core._STATTYPE_TO_AFFINITY[stattype] is "real":
+                    if core._STATTYPE_TO_AFFINITY[stattype] is 'real':
                         loom_output[row_index][col_index] = float(
                                 loom_output[row_index][col_index])
             return loom_output
 
-
     def logpdf_joint(self, bdb, generator_id, modelnos, rowid, targets,
             constraints):
-        """Analog is score"""
-        # ordering is stored somewhere in the data file
-        # TODO transform the data types ("string to int reprisentation") and do ordering
-        return 0
+        # TODO optimize bdb calls
+        def _convert_to_proper_stattype(bdb, population_id, colno, value):
+            """
+            Convert a value from whats given by the logpdf_joint
+            method parameters, to what loom can handle.
+            Ex. from an integer to real or from a string to an integer
+            """
+            if value is None:
+                return value
+
+            stattype = core.bayesdb_variable_stattype(
+                            bdb, population_id, colno)
+            if stattype == "numerical":
+                return float(value)
+
+            # Lookup the string encoding
+            if stattype == "categorical":
+                column_name = core.bayesdb_variable_name(bdb, population_id, colno)
+                encoding_list = self._get_encoding(bdb, generator_id)
+                column_encoding = next((x for x in encoding_list if x['name'] == column_name), None)
+                return column_encoding['symbols'][value]
+
+            return value
+
+        ordered_column_labels = self._get_order(bdb, generator_id)
+
+        and_case = collections.OrderedDict([(a, None) for a in ordered_column_labels])
+        conditional_case = collections.OrderedDict([(a, None) for a in ordered_column_labels])
+
+        population_id = core.bayesdb_generator_population(bdb, generator_id)
+        for (colno, value) in targets:
+            column_name = core.bayesdb_variable_name(bdb, population_id, colno)
+
+            and_case[column_name] = _convert_to_proper_stattype(bdb,
+                    population_id, colno, value)
+            conditional_case[column_name] = None
+        for (colno, value) in constraints:
+            column_name = core.bayesdb_variable_name(bdb,
+                    population_id, colno)
+            processed_value = _convert_to_proper_stattype(bdb,
+                    population_id, colno, value)
+
+            and_case[column_name] = processed_value
+            conditional_case[column_name] = processed_value
+
+        and_case = [a[1] for a in and_case.iteritems()]
+        conditional_case = [a[1] for a in conditional_case.iteritems()]
+
+        # TODO cache
+        qserver = loom.query.get_server(
+                self._get_path(core.bayesdb_generator_name(bdb, generator_id)))
+        return qserver.score(and_case) - qserver.score(conditional_case)
+
+    def _get_order(self, bdb, generator_id):
+        """Get the ordering of the columns according to loom"""
+        encoding_list = self._get_encoding(bdb, generator_id)
+        return [attr["name"] for attr in encoding_list]
+
+    def _get_encoding(self, bdb, generator_id):
+        """Get the encoding of our loom columns.
+        This includes the ordering of columns and the
+        integer encoding of categorical columns."""
+        # TODO cache
+        name = core.bayesdb_generator_name(bdb, generator_id)
+        with gzip.open(
+                os.path.join(self._get_path(name),
+                'ingest/encoding.json.gz')) as encoding_file:
+            return json.loads(encoding_file.read().decode('ascii'))
 
--- tests/test_loom_metamodel.py
+++ tests/test_loom_metamodel.py
@@ -13,15 +13,16 @@ def test_loom():
     prng = random.Random(time.time())
     with bayesdb_open(':memory:') as bdb:
         bayesdb_register_metamodel(bdb, LoomMetamodel())
-        bdb.sql_execute('create table t(x, y)')
+        bdb.sql_execute('create table t(x, y, z)')
         for x in xrange(1000):
-            bdb.sql_execute('insert into t(x, y) values(?, ?)',
-                    (random.randrange(0, 200), random.randrange(0, 100),))
-        bdb.execute('create population p for t(x numerical; y numerical)')
+            bdb.sql_execute('insert into t(x, y, z) values(?, ?, ?)',
+                    (random.randrange(0, 200), random.randrange(0, 100), 'a' if random.uniform(0, 1) > 0.5 else 'b'))
+        bdb.execute('create population p for t(x numerical; y numerical; z categorical)')
         bdb.execute('create generator g for p using loom')
         bdb.execute('initialize 1 model for g')
         bdb.execute('analyze g for 1 iteration wait')
-        #bdb.execute('estimate probability density of x = 50 and y = 50 from p').fetchall()
+        bdb.execute('estimate probability density of x = 50 by p').fetchall()
+        bdb.execute('estimate probability density of z = "a" by p').fetchall()
         print(bdb.execute('simulate x, y from p limit 10').fetchall())
         print(bdb.execute('estimate dependence probability from pairwise variables of p').fetchall())
 
