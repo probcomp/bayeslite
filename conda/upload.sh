696b2d516eba8b64586401734c7259d1b1376822
Aaron Vinson <avinson.public@gmail.com>
2017-10-20 16:28:41 -0700

    testing out checking for tags, etc.

---
+++ conda/upload.sh
@@ -0,0 +1,10 @@
+# if this build was invoked by a tag or a cron, then upload the package. otherwise exit.
+if [[ -n $TRAVIS_TAG ]]; then
+  conda install anaconda-client
+  anaconda -t $CONDA_UPLOAD_TOKEN upload -u $CONDA_USER ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+elif [[ TRAVIS_EVENT_TYPE == "cron "]]; then
+  conda install anaconda-client
+  anaconda -t $CONDA_UPLOAD_TOKEN upload -u $CONDA_USER -l nightly ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+else
+  exit 0
+fi
