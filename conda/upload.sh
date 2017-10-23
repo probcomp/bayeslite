e06e39385f57c526fa00c9f3825cc35c4c909942
Aaron Vinson <avinson.public@gmail.com>
2017-10-23 12:49:12 -0700

    add functionality to release conda packages from non-master branches

--- conda/upload.sh
+++ conda/upload.sh
@@ -4,7 +4,12 @@ set -ev
 # if this build was invoked by a tag or a cron, then upload the package. otherwise exit.
 if [ -n "${TRAVIS_TAG}" ]; then
   conda install anaconda-client
-  anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+  # if tag didn't come from master, add the "experimental" label
+  if [ $(git branch --contains tags/${TRAVIS_TAG}) = "master" ]; then
+    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+  else
+    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l experimental ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+  fi
 elif [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
   conda install anaconda-client
   anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l nightly ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
