44a4f5ad8c65fb8ee556f5c87574856bedc64dbb
Aaron Vinson <avinson.public@gmail.com>
2017-10-23 12:58:14 -0700

    change experimental label to dev because that makes more sense

--- conda/upload.sh
+++ conda/upload.sh
@@ -4,11 +4,11 @@ set -ev
 # if this build was invoked by a tag or a cron, then upload the package. otherwise exit.
 if [ -n "${TRAVIS_TAG}" ]; then
   conda install anaconda-client
-  # if tag didn't come from master, add the "experimental" label
+  # if tag didn't come from master, add the "dev" label
   if [ $(git branch --contains tags/${TRAVIS_TAG}) = "master" ]; then
     anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
   else
-    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l experimental ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
+    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l dev ~/miniconda/conda-bld/linux-64/bayeslite-*.tar.bz2 --force
   fi
 elif [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
   conda install anaconda-client
